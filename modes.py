e415344df26a72bffa4b5ad86d641d3f963d8148
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-02-01 14:32:32 +0300

    Update modes.py

--- modes.py
+++ modes.py
@@ -14,3 +14,28 @@ models.Index(fields=['-publish']),
 ]
 def __str__(self):
 return self.title
+
+
+from django.db import models
+from django.utils import timezone
+class Post(models.Model):
+class Status(models.TextChoices):
+DRAFT = 'DF', 'Draft'
+PUBLISHED = 'PB', 'Published'
+title = models.CharField(max_length=250)
+slug = models.SlugField(max_length=250)
+body = models.TextField()Building a Blog Application
+18
+publish = models.DateTimeField(default=timezone.now)
+created = models.DateTimeField(auto_now_add=True)
+updated = models.DateTimeField(auto_now=True)
+status = models.CharField(max_length=2,
+choices=Status.choices,
+default=Status.DRAFT)
+class Meta:
+ordering = ['-publish']
+indexes = [
+models.Index(fields=['-publish']),
+]
+def __str__(self):
+return self.title
