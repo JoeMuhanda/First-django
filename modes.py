510790621e1535a8a4980940e3d916ddf2eec4b6
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-02-03 16:55:33 +0300

    Update modes.py

--- modes.py
+++ modes.py
@@ -8,7 +8,7 @@ DRAFT = 'DF', 'Draft'
 PUBLISHED = 'PB', 'Published'
 title = models.CharField(max_length=250)
 slug = models.SlugField(max_length=250)
-author = models.ForeignKey(User,
+author = models.ForeignKey(User),
 on_delete=models.CASCADE,
 related_name='blog_posts'
 body = models.TextField()
