040c662e24724179606ad178d4011310742f4fc3
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-02-03 16:55:53 +0300

    Update modes.py

--- modes.py
+++ modes.py
@@ -15,7 +15,7 @@ body = models.TextField()
 publish = models.DateTimeField(default=timezone.now)
 created = models.DateTimeField(auto_now_add=True)
 updated = models.DateTimeField(auto_now=True)
-status = models.CharField(max_length=2,
+status = models.CharField(max_length=2,)
 choices=Status.choices,
 default=Status.DRAFT)
 class Meta:
